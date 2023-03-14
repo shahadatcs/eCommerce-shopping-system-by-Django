db31c28a2a0faaf41ff1447179647233da5612c9
shahadatcs <shahadat.pust@gmail.com>
2023-03-15 00:34:05 +0600

    update search

--- store/urls.py
+++ store/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('', views.store, name='store'),
     path('category/<slug:category_slug>/', views.store, name='product_by_category'),
     path('category/<slug:category_slug>/<slug:product_slug>/', views.product_detail, name='product_detail'),
-    path('search', views.search, name='search'),
+    path('search/', views.search, name='search'),
 ]
